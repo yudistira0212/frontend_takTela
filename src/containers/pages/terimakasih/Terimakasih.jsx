ff60dcc4e52272dea32ddcd561095da0d8442a30
Akmal Yudistira <akmalyudistira1212@gmail.com>
2023-10-09 15:01:33 +0900

    memperbaiki eror saat checkout

--- src/containers/pages/terimakasih/Terimakasih.jsx
+++ src/containers/pages/terimakasih/Terimakasih.jsx
@@ -55,69 +55,73 @@ const Terimakasih = () => {
       <div>
         <Navbar namaNav="Terimakasih" />
         <div className=" container pt-5" style={{ maxWidth: "500px" }}>
-          {dataRiwayat && (
-            <div
-              className="container  my-3 p-2 shadow-sm rounded-3 bg-body-tertiary"
-              style={{ maxWidth: "500px" }}
-              key={dataRiwayat.id}
-            >
-              <div className="fw-bold fs-4">Terimakasih Sudah Memesan</div>
-              <div>
-                <div className=" fw-bold ">
-                  TanggalPesan : {formatTanggal(dataRiwayat.waktuPesan)}{" "}
-                </div>
-                <div className=" fw-bold ">
-                  Jam : {formatJam(dataRiwayat.waktuPesan)}
-                </div>
-                <div className=" fw-bold ">
-                  Kede Pembelian : {dataRiwayat.code}
-                </div>
-              </div>
-              <div className="border-top">
-                {dataRiwayat.pesanan.map((value) => (
-                  <div key={value.id} className="my-2">
-                    <div>
-                      {value.nama} {value.variant}
-                    </div>
-                    <div className="d-flex justify-content-between  container-fluid">
-                      <div>
-                        {value.pesanan} X {formatToIDR(value.harga)}
-                      </div>
-                      <div> {formatToIDR(value.totalHargaPesanan)}</div>
-                    </div>
-                  </div>
-                ))}
-              </div>
-              <div className="border-top">
-                <div className=" ">Alamat : {dataRiwayat.alamat}</div>
+          {dataRiwayat &&
+            dataRiwayat.pesanan &&
+            dataRiwayat.pesanan.length > 0 && (
+              <div
+                className="container  my-3 p-2 shadow-sm rounded-3 bg-body-tertiary"
+                style={{ maxWidth: "500px" }}
+                key={dataRiwayat.id}
+              >
+                <div className="fw-bold fs-4">Terimakasih Sudah Memesan</div>
                 <div>
-                  Status :{" "}
-                  {dataRiwayat.flag ? "Dalam proses" : "Sudah di terima"}
+                  <div className=" fw-bold ">
+                    TanggalPesan : {formatTanggal(dataRiwayat.waktuPesan)}{" "}
+                  </div>
+                  <div className=" fw-bold ">
+                    Jam : {formatJam(dataRiwayat.waktuPesan)}
+                  </div>
+                  <div className=" fw-bold ">
+                    Kede Pembelian : {dataRiwayat.code}
+                  </div>
                 </div>
-                <div>Total Harga : {formatToIDR(dataRiwayat.totalHarga)} </div>
+                <div className="border-top">
+                  {dataRiwayat.pesanan.map((value) => (
+                    <div key={value.id} className="my-2">
+                      <div>
+                        {value.nama} {value.variant}
+                      </div>
+                      <div className="d-flex justify-content-between  container-fluid">
+                        <div>
+                          {value.pesanan} X {formatToIDR(value.harga)}
+                        </div>
+                        <div> {formatToIDR(value.totalHargaPesanan)}</div>
+                      </div>
+                    </div>
+                  ))}
+                </div>
+                <div className="border-top">
+                  <div className=" ">Alamat : {dataRiwayat.alamat}</div>
+                  <div>
+                    Status :{" "}
+                    {dataRiwayat.flag ? "Dalam proses" : "Sudah di terima"}
+                  </div>
+                  <div>
+                    Total Harga : {formatToIDR(dataRiwayat.totalHarga)}{" "}
+                  </div>
 
-                <div className="d-flex  justify-content-between  align-items-center ">
-                  <div>Lakukan konfirmasi agar pesanan di proses</div>
-                  <button
-                    className="btn btn-primary "
-                    onClick={() => {
-                      const openWhatsApp = () => {
-                        const message = `Halo min Saya ingin melakukan konfirmasi,\nKode pemesanan saya: ${dataRiwayat.code}\nEmail: ${dataRiwayat.email}`;
-                        const phoneNumber = "+6282239088465";
-                        const whatsappURL = `https://api.whatsapp.com/send?phone=${phoneNumber}&text=${encodeURIComponent(
-                          message
-                        )}`;
-                        window.open(whatsappURL, "_blank");
-                      };
-                      openWhatsApp();
-                    }}
-                  >
-                    Konfirmasi
-                  </button>
+                  <div className="d-flex  justify-content-between  align-items-center ">
+                    <div>Lakukan konfirmasi agar pesanan di proses</div>
+                    <button
+                      className="btn btn-primary "
+                      onClick={() => {
+                        const openWhatsApp = () => {
+                          const message = `Halo min Saya ingin melakukan konfirmasi,\nKode pemesanan saya: ${dataRiwayat.code}\nEmail: ${dataRiwayat.email}`;
+                          const phoneNumber = "+6282239088465";
+                          const whatsappURL = `https://api.whatsapp.com/send?phone=${phoneNumber}&text=${encodeURIComponent(
+                            message
+                          )}`;
+                          window.open(whatsappURL, "_blank");
+                        };
+                        openWhatsApp();
+                      }}
+                    >
+                      Konfirmasi
+                    </button>
+                  </div>
                 </div>
               </div>
-            </div>
-          )}
+            )}
         </div>
         <div
           className=" position-fixed bg-white border-top  container-fluid"
